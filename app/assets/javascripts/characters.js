f60e825c859a2bff52e17ca3bc1deda11e555aba
Raiet <raietsaeed@gmail.com>
2018-08-16 17:33:31 +0000

    added test button click function to ensure proper data captuer

--- app/assets/javascripts/characters.js
+++ app/assets/javascripts/characters.js
@@ -12,6 +12,16 @@ function attachListeners() {
     // Add button event listeners
     $("#previous-char").click(previousButtonClick);
     $("#next-char").click(nextButtonClick);
+    $("button").click(buttonTestClick);
+};
+
+// Temp button info to get "what was clicked"
+function buttonTestClick(){
+    console.log('Button clicked');
+    console.log('Current data = ', this);
+    const id = $(this).data("id");
+    const showId = $(this).data("show_id");
+    console.log(`Character id : ${id} | Show id : ${showId}`);
 };
 
 // Shuffle between characters in show -- Get previous character (when applicable)
