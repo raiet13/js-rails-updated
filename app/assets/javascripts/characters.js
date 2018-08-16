85df2f35965c1edc31a7bf72f85d1150451c98d4
Raiet <raietsaeed@gmail.com>
2018-08-16 17:18:01 +0000

    updated characters.js for button clicks

--- app/assets/javascripts/characters.js
+++ app/assets/javascripts/characters.js
@@ -2,5 +2,24 @@
 
 $(document).ready(function(){
     console.log('Character Controller Page');
+    attachListeners();
 });
 
+// Attach event listeners
+function attachListeners() {
+    console.log('Add click listeners');
+    
+    // Add button event listeners
+    $("#previous-char").click(previousButtonClick);
+    $("#next-char").click(nextButtonClick);
+};
+
+// Shuffle between characters in show
+function previousButtonClick(){
+    console.log('Render Previous Character Page');
+};
+
+// Shuffle between characters in show
+function nextButtonClick(){
+    console.log('Render Next Character Page');
+};
